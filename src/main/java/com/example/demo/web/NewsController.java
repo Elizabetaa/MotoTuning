ddcd543ea716e0338093613195c5755bf8d07a66
Elizabetaa <elizabetaa99@abv.bg>
2021-03-08 16:02:31 +0200

    start implementing news page html and css

---
+++ src/main/java/com/example/demo/web/NewsController.java
@@ -0,0 +1,15 @@
+package com.example.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/news")
+public class NewsController {
+
+    @GetMapping("/all")
+    public String all() {
+        return "news";
+    }
+}
