4ff933b460d7928301492616f888568a52db4641
Elizabetaa <elizabetaa99@abv.bg>
2021-03-20 16:09:50 +0200

    Implementing cloudinary

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.demo.service.impl;
 
-import com.cloudinary.Cloudinary;
 import com.example.demo.model.entity.UserEntity;
 import com.example.demo.model.entity.enums.RoleNameEnum;
 import com.example.demo.model.service.UserRegisterServiceModel;
@@ -9,12 +8,15 @@ import com.example.demo.service.CloudinaryService;
 import com.example.demo.service.RoleService;
 import com.example.demo.service.UserService;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.security.Principal;
 import java.util.List;
 
 @Service
@@ -25,14 +27,14 @@ public class UserServiceImpl implements UserService {
     private final PasswordEncoder passwordEncoder;
     private final CloudinaryService cloudinaryService;
 
-    public UserServiceImpl(UserRepository userRepository, RoleService roleService, ModelMapper modelMapper, PasswordEncoder passwordEncoder, CloudinaryService cloudinaryService) {
+    public UserServiceImpl(UserRepository userRepository, RoleService roleService, ModelMapper modelMapper, PasswordEncoder passwordEncoder,
+                           CloudinaryService cloudinaryService) {
         this.userRepository = userRepository;
         this.roleService = roleService;
         this.modelMapper = modelMapper;
         this.passwordEncoder = passwordEncoder;
         this.cloudinaryService = cloudinaryService;
     }
-
 
 
     @Override
@@ -47,8 +49,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public void addImage(MultipartFile imageUrl) throws IOException {
         MultipartFile img = imageUrl;
-        String url =  cloudinaryService.uploadImage(img);
-        UserEntity userEntity = this.userRepository.findById(1L).get();
+        String url = cloudinaryService.uploadImage(img);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        UserEntity userEntity = this.userRepository.findByEmail(userDetails.getUsername()).get();
         userEntity.setImageUrl(url);
         this.userRepository.save(userEntity);
     }
